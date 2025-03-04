6c70b41a1813212fda4aa5f6b9d43d00830773c6
isaias59 <127485411+isaias59@users.noreply.github.com>
2025-03-03 21:20:11 -0800

    Create graph.cpp

---
+++ src/graph.cpp
@@ -0,0 +1,124 @@
+#include "graph.h"
+
+void error(string msg) {
+    cerr << "Error: " << msg << endl;
+    exit(EXIT_FAILURE);
+}
+
+DisjointSet::DisjointSet(int numVertices) {
+    subsets.resize(numVertices);
+    for (int i = 0; i < numVertices; ++i) {
+        makeSet(i);
+    }
+}
+
+void DisjointSet::makeSet(Vertex x) {
+    subsets[x].parent = x;
+}
+
+Vertex DisjointSet::findSet(Vertex x) {
+    if (subsets[x].parent != x) {
+        subsets[x].parent = findSet(subsets[x].parent);
+    }
+    return subsets[x].parent;
+}
+
+void DisjointSet::unionSets(Vertex x, Vertex y) {
+    Vertex rootX = findSet(x);
+    Vertex rootY = findSet(y);
+    if (rootX != rootY) {
+        subsets[rootX].parent = rootY;
+    }
+}
+
+Graph Graph::sort_edges() const {
+    Graph sorted_graph = *this;
+    sort(sorted_graph.begin(), sorted_graph.end(), [](const Edge& a, const Edge& b) {
+        return a.weight < b.weight;
+        });
+    return sorted_graph;
+}
+
+VertexList Graph::edges_from(Vertex vertex) const {
+    VertexList vertices;
+    for (const auto& edge : *this) {
+        if (edge.u == vertex) {
+            vertices.push_back(edge.v);
+        }
+        else if (edge.v == vertex) {
+            vertices.push_back(edge.u);
+        }
+    }
+    return vertices;
+}
+
+EdgeList Kruskals(const Graph& G) {
+    EdgeList mst;
+    DisjointSet ds(G.numVertices);
+    Graph sorted_edges = G.sort_edges();
+
+    for (const auto& edge : sorted_edges) {
+        if (ds.findSet(edge.u) != ds.findSet(edge.v)) {
+            mst.push_back(edge);
+            ds.unionSets(edge.u, edge.v);
+        }
+    }
+    return mst;
+}
+
+int sum_weights(const EdgeList& L) {
+    return accumulate(L.begin(), L.end(), 0, [](int sum, const Edge& e) {
+        return sum + e.weight;
+        });
+}
+
+void file_to_graph(string filename, Graph& G) {
+    ifstream file(filename);
+    if (!file) {
+        error("File not found: " + filename);
+    }
+    file >> G;
+}
+
+VertexList dfs(const Graph& graph, Vertex startVertex) {
+    VertexList visited;
+    stack<Vertex> s;
+    s.push(startVertex);
+
+    while (!s.empty()) {
+        Vertex v = s.top();
+        s.pop();
+        if (find(visited.begin(), visited.end(), v) == visited.end()) {
+            visited.push_back(v);
+            for (auto&& neighbor : graph.edges_from(v)) {
+                s.push(neighbor);
+            }
+        }
+    }
+    return visited;
+}
+
+VertexList bfs(const Graph& graph, Vertex startVertex) {
+    VertexList visited;
+    queue<Vertex> q;
+    q.push(startVertex);
+
+    while (!q.empty()) {
+        Vertex v = q.front();
+        q.pop();
+        if (find(visited.begin(), visited.end(), v) == visited.end()) {
+            visited.push_back(v);
+            for (auto&& neighbor : graph.edges_from(v)) {
+                q.push(neighbor);
+            }
+        }
+    }
+    return visited;
+}
+
+string get_arg(int argc, char* argv[], string def) {
+    if (argc < 2) {
+        return def;
+    }
+    return argv[1];
+}
